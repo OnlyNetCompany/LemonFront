b57d1df6c76fb3e1542fca63e56277f59a69b9d6
Francisco Valero <129222208+franvale581@users.noreply.github.com>
2025-11-07 17:22:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ window.addEventListener("DOMContentLoaded", () => {
     setTimeout(() => btn.classList.add("expand"), 300);
 });
 
+
+const BASE_URL = "https://informed-bouquet-1f02098133.strapiapp.com";
+
 // ----------- OBTENER SIGUIENTE NÚMERO DE STRAPI -------------
 async function getNextPhone() {
     try {
-        const res = await fetch("https://elegant-friendship-1ee1f8c64e.strapiapp.com/phones/next");
+        const BASE_LINK = BASE_URL;
+        const res = await fetch(`${BASE_LINK}/phones/next`);
         const data = await res.json();
 
         if (!data.number) {
@@ -41,7 +45,8 @@ if (whatsappBtn) {
 
         // Redirigir a WhatsApp
         // Redirigir a WhatsApp en nueva pestaña
-        const message = encodeURIComponent("Hola, quiero un usuario");
-        window.open(`https://wa.me/${number}?text=${message}`, "_blank");
+        const message = encodeURIComponent("Hola, quiero un usuario!");
+        window.location.href = `https://wa.me/${number}?text=${message}`;
+
     });
 }
